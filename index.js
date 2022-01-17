711b63b9bb26a36494550cc5a0cf570d4b844358
LidaChk <lvglazko@gmail.com>
2022-01-17 18:22:44 +0300

    feat: hero

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ window.onload = function() {
 
   document.querySelectorAll("a.language__link").forEach(element =>
     element.addEventListener("click", function() {
-      if (!this.classList.contains('active')) {
-        document.querySelector("a.language__link.active").classList.remove("active");
-        this.classList.add("active");
+      if (!this.classList.contains('switch_on')) {
+        document.querySelector("a.language__link.switch_on").classList.remove("switch_on");
+        this.classList.add("switch_on");
       }
       return true;
     }));
