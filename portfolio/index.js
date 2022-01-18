dcff25edd173fd68a5425a93a7e7179c1249812e
LidaChk <lvglazko@gmail.com>
2022-01-18 13:34:46 +0300

    fix: buttons;

--- portfolio/index.js
+++ portfolio/index.js
@@ -3,24 +3,25 @@
 window.onload = function() {
 
   document.querySelectorAll("a.language__link").forEach(element =>
-    element.addEventListener("click", function() {
+    element.addEventListener("click", function langSwitch() {
       if (!this.classList.contains('switch_on')) {
         document.querySelector("a.language__link.switch_on").classList.remove("switch_on");
         this.classList.add("switch_on");
       }
-      return true;
+      return false;
     }));
 
 
-    document.querySelector('#portfolio').querySelectorAll('button').forEach(element =>
-      element.addEventListener("click", function() {
+    document.querySelector('#portfolio').querySelectorAll('button.btn_basic').forEach(element =>
+      element.addEventListener("click", function btnChangeColor() {
         if (this.classList.contains('btn_blacky')) {
-          let el = document.querySelector('#portfolio').querySelector("button.btn_goldy");
+          let el = document.querySelector('#portfolio').querySelector('button.btn_goldy');
           el.classList.add('btn_blacky');
           el.classList.remove('btn_goldy');
           this.classList.add('btn_goldy');
         }
-        return true;
+        console.log('btnChangeColor');
+        return false;
       }));
 };
 
