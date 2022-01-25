3e37403953c1bbb5b262bb94edaae7ad8cf5a90e
LidaChk <lvglazko@gmail.com>
2022-01-25 09:15:15 +0300

    feat: burger without menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,6 +1,4 @@
-
-
-window.onload = function() {
+window.onload = function () {
 
   document.querySelectorAll("a.language__link").forEach(element =>
     element.addEventListener("click", function langSwitch() {
@@ -10,26 +8,32 @@ window.onload = function() {
       }
       return false;
     }));
-    
 
-    document.querySelector('#portfolio').querySelectorAll('button.btn_basic').forEach(element =>
-      element.addEventListener("click", function btnChangeColor() {
-        if (this.classList.contains('btn_blacky')) {
-          let el = document.querySelector('#portfolio').querySelector('button.btn_goldy');
-          el.classList.togle('btn_blacky');
-          el.classList.togle('btn_goldy');
-          this.classList.togle('btn_goldy');
-        }
-        console.log('btnChangeColor');
-        return false;
-      }));
+
+  document.querySelector('#portfolio').querySelectorAll('button.btn_basic').forEach(element =>
+    element.addEventListener("click", function btnChangeColor() {
+      if (this.classList.contains('btn_blacky')) {
+        let el = document.querySelector('#portfolio').querySelector('button.btn_goldy');
+        el.classList.togle('btn_blacky');
+        el.classList.togle('btn_goldy');
+        this.classList.togle('btn_goldy');
+      }
+      console.log('btnChangeColor');
+      return false;
+    }));
+
+  document.querySelector('.burger_container').addEventListener('click', function () {
+    this.classList.toggle('change')
+  });
 };
 
 /* let res = 'Оценка 92\n1.Вёрстка валидная +10\n2.Вёрстка семантическая +20\n3.Вёрстка соответствует макету(частично) +40\n4.Требования к css + 12\n5.Интерактивность, реализуемая через css +20'
 console.log(res); */
 
-function strCount(obj){
- for (var prop in obj) {
-  console.log("obj." + prop + " = " + obj[prop]);
-}
+console.log("Буду благодарна, если проверите мою работу поздже. https://discord.gg/FNfcbgjn" );
+
+function strCount(obj) {
+  for (var prop in obj) {
+    console.log("obj." + prop + " = " + obj[prop]);
+  }
 }
