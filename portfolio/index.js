b3c139d3e7aa02403c94e444d4471d18576d4719
LidaChk <lvglazko@gmail.com>
2022-02-01 12:06:22 +0300

    fix: benefits

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,11 +1,11 @@
  import i18Obj from './js/translate.js';
-
+ const aOpacityItems = ['p', 'h1', 'h2', 'h3', 'a', 'button', '.price__text']
 
 
  // Theme Loader&Switcher
 
- if (!localStorage.getItem('data-theme')) localStorage.setItem('data-theme', 'dark');
- document.documentElement.setAttribute("data-theme", localStorage.getItem('data-theme'));
+ /*  if (!localStorage.getItem('data-theme')) localStorage.setItem('data-theme', 'dark');
+  document.documentElement.setAttribute("data-theme", localStorage.getItem('data-theme')); */
 
  function switchTheme() {
    localStorage.setItem('data-theme', (localStorage.getItem('data-theme') == 'dark') ? 'light' : 'dark');
@@ -19,75 +19,86 @@
 
  if (!localStorage.getItem('cur-season')) localStorage.setItem('cur-season', 'autumn');
 
-function setSeason(sSeason) {
-  
-  document.querySelectorAll('.pf_img.active').forEach(e => e.classList.remove('active'));
-  document.querySelectorAll('.pf_img.' + sSeason).forEach(e => e.classList.add('active'));
-  localStorage.setItem('cur-season', sSeason);
-  document.querySelectorAll('button').
-  forEach(b => {
-    if (b.dataset.season == sSeason) b.classList.add('btn_active')
-  });
-}
+ function setSeason(sSeason) {
 
-function changeSeason(event) {
-  let element = event.target;
-  if (element.type == 'button') {
-    document.querySelector('.portfolio_btns_lst').querySelectorAll('button').
-    forEach(b => b.classList.remove('btn_active'));
-    setSeason(element.dataset.season);
-  }
-}
+   document.querySelectorAll('.pf_img.active').forEach(e => e.classList.remove('active'));
+   document.querySelectorAll('.pf_img.' + sSeason).forEach(e => e.classList.add('active'));
+   localStorage.setItem('cur-season', sSeason);
+   document.querySelectorAll('button').
+   forEach(b => {
+     if (b.dataset.season == sSeason) b.classList.add('btn_active')
+   });
+ }
+
+ function changeSeason(event) {
+   let element = event.target;
+   if (element.type == 'button') {
+     document.querySelector('.portfolio_btns_lst').querySelectorAll('button').
+     forEach(b => b.classList.remove('btn_active'));
+     setSeason(element.dataset.season);
+   }
+ }
 
  //------------------------------------
+ function langSwitcher(to) {
+  aOpacityItems.forEach(a => document.querySelectorAll(a).forEach(el => el.style.opacity = 0));
+   const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+   let from = (to == 'ru') ? 'en' : 'ru';
+   /* console.log(from, ' => ', to); */
+   while (walker.nextNode()) {
+     let text = walker.currentNode.textContent.trim();
+     for (let [key, value] of Object.entries(i18Obj[from])) {
+       if (text === value.trim()) {
+         walker.currentNode.textContent = i18Obj[to][key];
+       }
+     }
+   }
+   document.querySelectorAll(".form_input").forEach(el => {
+     let text = el.placeholder;
+     for (let [key, value] of Object.entries(i18Obj[from])) {
+       if (text === value.trim()) {
+         el.placeholder = i18Obj[to][key];
+       }
+     }
+   });
+   aOpacityItems.forEach(a => document.querySelectorAll(a).forEach(el => el.style.opacity = 1));
+   document.querySelectorAll("a.language__link").forEach(el => el.classList.toggle('switch_on'));
+
+   localStorage.setItem('cur-lang', to);
+ }
+
+
+ window.onload = function () {
+
+
+   setSeason(localStorage.getItem('cur-season'));
+   //Language utils
+
+   if (!localStorage.getItem('cur-lang')) localStorage.setItem('cur-lang', 'en');
+   if (localStorage.getItem('cur-lang') == 'ru') langSwitcher('ru');
+
+   aOpacityItems.forEach(a => document.querySelectorAll(a).forEach(el => el.style.opacity = 1));
+
+   document.querySelectorAll("a.language__link").forEach(element =>
+     element.addEventListener("click", function langSwitch() {
+       if (!this.classList.contains('switch_on')) {
+         let switchTo = this.innerText.toLocaleLowerCase();
+         langSwitcher(switchTo);
+       }
+       return false;
+     }));
+
+   document.querySelector('div.theme__switch').addEventListener('click', switchTheme);
+   document.querySelector('.portfolio_btns_lst').addEventListener('click', changeSeason)
 
 
 
-window.onload = function () {
-
-
-  setSeason(localStorage.getItem('cur-season'));
-//Language utils
-
-  if (!localStorage.getItem('cur-lang')) localStorage.setItem('cur-lang', 'en');
-
-  function langSwitcher(to) {
-    const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
-    let from = (to == 'ru') ? 'en' : 'ru';
-    /* console.log(from, ' => ', to); */
-    while (walker.nextNode()) {
-      let text = walker.currentNode.textContent.trim();
-      for (let [key, value] of Object.entries(i18Obj[from])) {
-        if (text === value.trim()) {
-          walker.currentNode.textContent = i18Obj[to][key];
-        }
-      }
-    }
-    document.querySelectorAll("a.language__link").forEach(el => el.classList.toggle('switch_on'));
-    localStorage.setItem('cur-lang', to);
-  }
-  if (localStorage.getItem('cur-lang') == 'ru') langSwitcher('ru');
-  document.querySelector('body').style.opacity = 1
-
-  document.querySelectorAll("a.language__link").forEach(element =>
-    element.addEventListener("click", function langSwitch() {
-      if (!this.classList.contains('switch_on')) {
-        let switchTo = this.innerText.toLocaleLowerCase();
-        langSwitcher(switchTo);
-      }
-      return false;
-    }));
-
-  document.querySelector('div.theme__switch').addEventListener('click', switchTheme);
-  document.querySelector('.portfolio_btns_lst').addEventListener('click', changeSeason)
-
-
-
-//NavBar Utils
+   //NavBar Utils
    function OpenNavBar() {
 
-     let arrTabletItems = ['.navburger__wrapper', '.header__container', '.burger_container', 'h2', 
-     '.wrapper', '.video_player', 'form__contacts', '.hero__content_box', '.form__contacts', '.skills', '.navigation__link', '.btn_basic']
+     let arrTabletItems = ['.navburger__wrapper', '.header__container', '.burger_container', 'h2',
+       '.wrapper', '.video_player', 'form__contacts', '.hero__content_box', '.form__contacts', 
+       '.skills', '.navigation__link', '.btn_basic', '.container']
 
      arrTabletItems.forEach(x => document.querySelectorAll(x).forEach(el => el.classList.toggle('tablet_navbar')));
 
